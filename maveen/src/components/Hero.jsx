836fa1943c3f4548013c1f2f02dc9da2f2a2eaad
Sargunam <tsargunampu@gmail.com>
2025-08-30 16:52:15 +0530

    Adjusted hero.jsx and added css

--- maveen/src/components/Hero.jsx
+++ maveen/src/components/Hero.jsx
@@ -8,10 +8,10 @@ const Hero = () => (
       
       {/* Left Side - Text */}
       <div className="hero-text text-center text-lg-start flex-fill mb-4 mb-lg-0">
-        <h1 className="fw-bold display-4">
-          MAVEEN TEAM
-        </h1>
-        <p className="lead">Excellence to Execute & Care</p>
+        <h6 className="fw-bold display-2 ">
+          Excellence to Execute & Care
+        </h6>
+        {/* <p className="lead">Excellence to Execute & Care</p> */}
         <p>
           Advisory Services & Business Process Improvement. <br />
           We help organizations achieve performance through specialized training,
