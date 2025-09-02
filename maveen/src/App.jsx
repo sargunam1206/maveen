fb695ef761451f01829f2a63f268fc24976eda02
Sargunam <tsargunampu@gmail.com>
2025-09-02 15:15:08 +0530

    Added Animations for service page and active bar

--- maveen/src/App.jsx
+++ maveen/src/App.jsx
@@ -1,39 +1,27 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
-import HomePage from "./pages/HomePage"; // adjust path if needed
+import HomePage from "./pages/HomePage";
 import Services from "./pages/Services"; 
 import About from './pages/About'
 import Footer from "./components/Footer";
 import Contact from "./pages/Contact";
 
-
-// Simple pages
-// const Home = () => <h1 className="p-4">🏠 Welcome to Home</h1>;
-// const About = () => <h1 className="p-4">ℹ️ About Us</h1>;
-// const Services = () => <h1 className="p-4">🛠 Services</h1>;
-// const Customers = () => <h1 className="p-4">👥 Customers</h1>;
-// const Contact = () => <h1 className="p-4">📞 Contact</h1>;
-
-
 function App() {
   return (
     <>
-    <Router>
-      <Navbar />
-      <main>
-        <Routes>
-          <Route path="/" element={<HomePage />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/services" element={<Services />} />
-          {/* <Route path="/customers" element={<Customers />} /> */}
-          <Route path="/contact" element={<Contact />} />
-        </Routes>
-      </main>
-      
-    </Router>
-    <Footer/>
-
+      <Router>
+        <Navbar />
+        <main>
+          <Routes>
+            <Route path="/" element={<HomePage />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/services" element={<Services />} />
+            <Route path="/contact" element={<Contact />} />
+          </Routes>
+        </main>
+      </Router>
+      <Footer />
     </>
   );
 }
